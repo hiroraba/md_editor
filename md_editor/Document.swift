d10bff292222aa56de6a31b388dd16f61b51e4fa
hiroraba <xornelius0313@gmail.com>
2025-04-09 00:27:55 +0900

    マークダウンをリスト、追加、削除する機能を追加

---
+++ md_editor/Document.swift
@@ -0,0 +1,16 @@
+//
+//  Document.swift
+//  md_editor
+//
+//  Created by 松尾宏規 on 2025/04/08.
+//
+
+import RealmSwift
+import Foundation
+
+final class Document: Object, Identifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title: String
+    @Persisted var content: String
+    @Persisted var createdAt: Date
+}
